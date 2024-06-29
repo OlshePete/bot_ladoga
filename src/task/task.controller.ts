fdadd3985618ddfea62872755f46b7972346eb31
OlshePete <olshevskii.pv@gmail.com>
2024-06-29 21:13:02 +0300

    try fix controller path issue

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { TaskService } from './task.service';
 
-@Controller('task')
+@Controller('bot/task')
 export class TaskController {
   constructor(private readonly taskService:TaskService ) {}
     @Get('add')
